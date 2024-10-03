69dd6fc8db4faa75d347dd81b897fc3d74783583
Lexciese <falah.naufal.zk@gmail.com>
2024-10-03 21:06:49 +0700

    p

--- bringup/launch/rrbot_gazebo.launch.py
+++ bringup/launch/rrbot_gazebo.launch.py
@@ -87,7 +87,7 @@ def generate_launch_description():
     node_robot_state_publisher = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
-        output="both",
+        output="screen",
         parameters=[robot_description],
     )
 
